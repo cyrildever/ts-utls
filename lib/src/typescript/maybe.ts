d2b15daa6acd935e6eb221293ab22958e4126eb6
Cyril Dever <cdever@pep-s.com>
2022-12-07 19:19:58 +0100

    add light monad implementation to maybe

--- lib/src/typescript/maybe.ts
+++ lib/src/typescript/maybe.ts
@@ -23,6 +23,17 @@ SOFTWARE.
 */
 
 export interface Maybe<T> {
+  /* Monad implementation */
+  ap<V>(maybeFn: Maybe<(val: T) => V>): Maybe<V>
+  bind<V>(fn: (val: T) => Maybe<V>): Maybe<V>
+  flatMap<V>(fn: (val: T) => Maybe<V>): Maybe<V>
+  chain<V>(fn: (val: T) => Maybe<V>): Maybe<V>
+  join<V>(): Maybe<V> // If T extends Maybe<V>
+  map<V>(fn: (val: T) => V): Maybe<V>
+  takeLeft(m: Maybe<T>): Maybe<T>
+  takeRight(m: Maybe<T>): Maybe<T>
+
+  /* Maybe specifics */
   getOrElse(val: T): T
   isNone(): boolean
   isSome(): boolean
@@ -40,6 +51,60 @@ class MaybeImpl<T> implements Maybe<T> {
     }
     this.val = val
   }
+
+  /* Monad implementation */
+
+  ap<V>(maybeFn: Maybe<(val: T) => V>): Maybe<V> {
+    if (this.hasValue) {
+      const value = this.val
+      return maybeFn.map(function (fn) {
+        return fn(value)
+      })
+    } else {
+      return None<V>()
+    }
+  }
+
+  bind<V>(bindFn: (val: T) => Maybe<V>): Maybe<V> {
+    return this.hasValue ? bindFn(this.val) : None<V>()
+  }
+
+  flatMap<V>(flatMapFn: (val: T) => Maybe<V>): Maybe<V> {
+    return this.hasValue ? flatMapFn(this.val) : None<V>()
+  }
+
+  chain<V>(chainFn: (val: T) => Maybe<V>): Maybe<V> {
+    return this.hasValue ? chainFn(this.val) : None<V>()
+  }
+
+  join<V>(): Maybe<V> {
+    return this.flatMap(idFunction)
+  }
+
+  map<V>(mapFn: (val: T) => V): Maybe<V> {
+    try {
+      const m = mapFn(this.val)
+      return m !== undefined ? Some(m) : None<V>()
+    } catch (e) {
+      return None<V>()
+    }
+  }
+
+  takeLeft(m: Maybe<T>): Maybe<T> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return apply2(this, m, function (a: Maybe<T>, b: Maybe<T>): Maybe<T> {
+      return a
+    })
+  }
+
+  takeRight(m: Maybe<T>): Maybe<T> {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return apply2(this, m, function (a: Maybe<T>, b: Maybe<T>): Maybe<T> {
+      return b
+    })
+  }
+
+  /* Maybe specifics */
 
   getOrElse(val: T): T {
     return this.hasValue ? this.val : val
@@ -61,6 +126,27 @@ class MaybeImpl<T> implements Maybe<T> {
     }
   }
 }
+
+const apply2 = <T>(a1: Maybe<T>, a2: Maybe<T>, f: (a: Maybe<T>, b: Maybe<T>) => Maybe<T>): Maybe<T> =>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  a2.ap(a1.map(curry(f, [])))
+
+const curry = (fn: any, args: Array<any>) => {
+  return function () {
+    const args1 = args.concat(getArgs(arguments)) // eslint-disable-line prefer-rest-params
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    return args1.length >= fn.length ?
+      // eslint-disable-next-line prefer-spread, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      fn.apply(null, args1.slice(0, args1.length)) :
+      curry(fn, args1)
+  }
+}
+
+const getArgs = (args: IArguments): Array<any> =>
+  Array.prototype.slice.call(args)
+
+const idFunction = (value: any): any =>
+  value
 
 const isNothing = (val: any): boolean =>
   val === null || val === undefined
