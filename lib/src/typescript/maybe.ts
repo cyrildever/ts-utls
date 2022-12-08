ecf3a55f3f9e6261b467a1cc48e2a492eb6981cf
Cyril Dever <cdever@pep-s.com>
2022-12-08 11:28:34 +0100

    fixes for null

--- lib/src/typescript/maybe.ts
+++ lib/src/typescript/maybe.ts
@@ -35,8 +35,8 @@ export interface Maybe<T> {
 
   /* Maybe specifics */
   cata<Z>(none: () => Z, some: (val: T) => Z): Z
-  filter<U extends T>(fn: (val: T) => val is U): Maybe<U>
-  filter(fn: (val: T) => boolean): Maybe<T>
+  filter<U extends T>(fn: (val: T | null) => val is U): Maybe<U>
+  filter(fn: (val: T | null) => boolean): Maybe<T>
   fold<V>(val: V): (fn: (val: T) => V) => V
   forEach(fn: (val: T) => void): void
 
@@ -116,7 +116,7 @@ class MaybeImpl<T> implements Maybe<T> {
     return this.isSome() ? some(this.val) : none()
   }
 
-  filter(fn: (val: T) => boolean): Maybe<T> {
+  filter(fn: (val: T | null) => boolean): Maybe<T> {
     const self = this // eslint-disable-line @typescript-eslint/no-this-alias
     return self.flatMap(function (a: T): Maybe<T> {
       return fn(a) ? self : None<T>()
@@ -188,7 +188,7 @@ export const None = <T>(): Maybe<T> =>
   new MaybeImpl(false, null as T)
 
 export const Maybe = {
-  fromNull: function <T>(val: T): Maybe<T> {
-    return isNothing(val) ? None<T>() : Some(val)
+  fromNull: function <T>(val: T | null): Maybe<T> {
+    return isNothing(val) ? None<T>() : Some<T>(val as T)
   }
 }
